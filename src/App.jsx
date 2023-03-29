fbe5cf173bd20b5e8cb0887cc3e4791e7998433f
guilh <guilherme00rogerio@gmail.com>
2023-03-29 20:54:29 -0300

    Inserindo pagina principal do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import './App.css';
 import KanbanLogo from './img/logo.svg'
+import Boards from './img/boards.svg'
+import Equipes from './img/equipes.svg'
+import Relatorios from './img/relatorios.svg'
+import Ajustes from './img/ajustes.svg'
 
 function App() {
   return (
     <div className="App">
-      <img src={KanbanLogo} alt="Logo do Kanban" />
+      <div className="kanban-menu">
+        <img src={KanbanLogo} alt="Logo do Kanban" />
+        <div className="options-container">
+          <a href="#">
+            <img src={Boards} alt="" />
+            <p>Boards</p>
+          </a>
+          <a href="#">
+            <img src={Equipes} alt="" />
+            <p>Equipes</p>
+          </a>
+          <a href="#">
+            <img src={Relatorios} alt="" />
+            <p>Relatórios</p>
+          </a>
+          <a href="#">
+            <img src={Ajustes} alt="" />
+            <p>Ajustes</p>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
