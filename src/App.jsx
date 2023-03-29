525270da56bae3eeeace91cd5a99807c29a8a00a
guilh <guilherme00rogerio@gmail.com>
2023-03-29 20:18:00 -0300

    Inserindo a logo como img

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import KanbanLogo from './img/logo.svg'
 
 function App() {
   return (
     <div className="App">
+      <img src={KanbanLogo} alt="Logo do Kanban" />
     </div>
   );
 }
