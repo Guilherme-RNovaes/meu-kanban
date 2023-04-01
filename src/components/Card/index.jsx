641e3d7772cffe790fa7f7f22c4f94d0a3a13490
guilh <guilherme00rogerio@gmail.com>
2023-04-01 18:40:01 -0300

    Efetuando testes nos primeiros cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,11 @@ import './styles.css'
 function Card() {
   return (
     <div className='card-container'>
-        
+        <div className="card-content">
+          <p>texto 1</p>
+          <p>texto 2</p>
+          <p>texto 3</p>
+        </div>
     </div>
   )
 }
